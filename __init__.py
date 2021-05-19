65b2142fc96cdae52f0eaee4160c22b2573bc8f4
Charlotte <charlotte@traumfabrikat.de>
2021-05-19 21:58:06 +0200

    test if installing from Git works

--- __init__.py
+++ __init__.py
@@ -1,4 +1,6 @@
+from adapt.intent import IntentBuilder
 from mycroft import MycroftSkill, intent_file_handler
+from mycroft.skills.context import adds_context, removes_context
 
 
 class TestSkill(MycroftSkill):
@@ -8,6 +10,12 @@ class TestSkill(MycroftSkill):
     @intent_file_handler('skill.test.intent')
     def handle_skill_test(self, message):
         self.speak_dialog('skill.test')
+
+    @intent_handler(IntentBuilder("TeaIntent").require("bring").require("tea"))
+    @adds_context("MilkContext")
+    def handle_tea_intent(self, message):
+        self.milk = False
+        self.speak("Of course, would you like Milk with that?", expect_response=True)
 
 
 def create_skill():
