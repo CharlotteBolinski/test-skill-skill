0e96e212c40be8cd5d1d92ba62ac8367bb442eaa
Charlotte <charlotte@traumfabrikat.de>
2021-05-20 21:34:02 +0200

    test context

--- __init__.py
+++ __init__.py
@@ -16,6 +16,12 @@ class TestSkill(MycroftSkill):
         self.log.info("This is an info level log message.")
         self.speak("Our first test should be simple", expect_response=True)
 
+    @intent_handler(IntentBuilder('ok').require('okay'))
+    def handle_hello_world_intent(self, message):
+        self.log.info("This is an info level log message2.")
+        self.speak("Let us start: what is pi?", expect_response=True)
+
+
 def create_skill():
     return TestSkill()
 
