bf3e5f3f3ba110b6c65104c049b7ca089db34c60
Charlotte <charlotte@traumfabrikat.de>
2021-05-20 21:56:27 +0200

    test context

--- __init__.py
+++ __init__.py
@@ -10,17 +10,27 @@ class TestSkill(MycroftSkill):
     def handle_skill_test(self, message):
         self.speak_dialog('skill.test')
 
-    @intent_handler(IntentBuilder('HelloTest').require('HappyTest'))
-    @adds_context('HappyContext')
+    @intent_handler(IntentBuilder('StartTest').require('StartTest'))
+    @adds_context('StartContext')
     def handle_hello_test_intent(self, message):
+        self.starttest = False
         self.log.info("This is an info level log message.")
         self.speak("Our first test should be simple", expect_response=True)
 
-    @intent_handler(IntentBuilder('OkayStartTest').require('OkayTest').require('HappyContext').build())
-    @removes_context('HappyContext')
+    @intent_handler(IntentBuilder('OkayStartTest').require('OkayTest').require('StartContext').build())
+    @removes_context('StartContext')
     @adds_context('TestContext')
     def handle_okay_test_intent(self, message):
+        self.starttest = True
         self.speak("What are the 20 first digits of pi?", expect_response=True)
+
+    @intent_handler(IntentBuilder("GoOnTest").require("OneAnswer").require("TestContext").build())
+    @removes_context('TestContext')
+    def handle_one_test_intent(self, message):
+        if self.starttest:
+            self.speak("That is the wrong answer")
+        else:
+            self.speak("That might be right")
 
 def create_skill():
     return TestSkill()
