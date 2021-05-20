6f14bde0658e7bfcddd667f2cb779ac79d453727
Charlotte <charlotte@traumfabrikat.de>
2021-05-20 20:12:37 +0200

    test if installing from Git works

--- __init__.py
+++ __init__.py
@@ -3,7 +3,6 @@ from mycroft import MycroftSkill, intent_file_handler
 class TestSkill(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
-        self.flavors = ['vanilla', 'chocolate', 'mint']
 
     @intent_file_handler('skill.test.intent')
     def handle_skill_test(self, message):
